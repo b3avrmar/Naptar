ef1f3ede87e308c39a36f95586558195dfb9b1f5
b3avrmar <b3avrmar@vasvari.org>
2026-01-26 09:02:56 +0100

    User selection

---
+++ naptar/naptar/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace naptar
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Felhasznalo();
+        }
+
+        static string Felhasznalo()
+        {
+            while (true)
+            {
+                Console.Write("Adja meg a felhasználot: ");
+                string input = Console.ReadLine();
+
+                switch (input)
+                {
+                    case "Apa":
+                        return "Apa";
+                    case "Anya":
+                        return "Anya";
+                    default:
+                        Console.WriteLine("Hibás felhasználó, próbálja újra.");
+                        Console.ReadKey();
+                        Console.Clear();
+                        break;
+                }
+            }
+        }
+
+        public struct Datetime
+        {
+            
+        }
+    }
+}
